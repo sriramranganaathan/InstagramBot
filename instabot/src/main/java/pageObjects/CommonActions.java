e0008787300394e6bfa96649c874c1d553b636de
RSRIRA26 <RSRIRA26@WGC101BPSS0X2.chennaigtbc.ford.com>
2019-11-14 00:04:05 +0530

    adding files

--- instabot/src/main/java/pageObjects/CommonActions.java
+++ instabot/src/main/java/pageObjects/CommonActions.java
@@ -1,5 +1,6 @@
 package pageObjects;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -8,10 +9,16 @@ import readLocator.Locator;
 
 public class CommonActions extends BaseClass{
 	
+	static Logger log = Logger.getLogger(CommonActions.class);
+	
 	By homePageIcon = By.xpath(Locator.getLocator("commonAction_homePageIcon"));
 	By notificationPanelText = By.xpath(Locator.getLocator("commonAction_turnNotificationOnOrOffPanelText"));
 	By turnOffNotification = By.xpath(Locator.getLocator("commonAction_turnOffNotification"));
 	By turnOnNotification = By.xpath(Locator.getLocator("commonAction_turnOnNotification"));
+	By loginLink = By.xpath(Locator.getLocator("commonAction_loginLink"));
+	By userName = By.xpath(Locator.getLocator("commonAction_userName"));
+	By passwordd = By.xpath(Locator.getLocator("commonAction_password"));
+	By loginButton = By.xpath(Locator.getLocator("commonAction_loginButton"));
 	
 	public CommonActions(WebDriver driver){
 		super(driver);
@@ -32,8 +39,7 @@ public class CommonActions extends BaseClass{
 			}
 		}
 		catch(Exception e){
-			System.out.println("Cannot find notification panel");
-			System.out.println(e);
+			log.error("Cannot find notification panel", e);
 		}
 	}
 	
@@ -44,9 +50,15 @@ public class CommonActions extends BaseClass{
 			}
 		}
 		catch(Exception e){
-			System.out.println("Cannot find notification panel");
-			System.out.println(e);
+			log.error("Cannot find notification panel", e);
 		}
+	}
+	
+	public void loginToUserAccount(String url, String username, String password){
+		driver.navigate().to(url);
+		type(userName, username);
+		type(passwordd, password);
+		click(loginButton);
 	}
 
 }
